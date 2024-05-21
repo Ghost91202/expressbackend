91f099ee00180fe71373319f280f13a4362d8e20
ghost91202 <karansharma8543@gmail.com>
2024-05-21 19:08:32 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env || 4000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
 app.get('/login', (req, res) => {
-  res.send('login')
-})
+  res.send('login');
+});
 
 app.get('/signup', (req, res) => {
-  res.send('signup')
-})
+  res.send('signup');
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-app.use(express.static('public'))
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
