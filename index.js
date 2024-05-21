f9085296069639ecfb9bf06137f1cd77f66760da
ghost91202 <karansharma8543@gmail.com>
2024-05-21 19:53:36 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 4000;
+const express = require('express'); // Imports the Express framework
+const app = express(); // Creates an Express application instance
 
+const port = 4000; // Uses the `PORT` environment variable if set, otherwise defaults to 4000
 
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!'); // Sends the "Hello World!" response for the root path (`/`)
 });
 
 app.get('/login', (req, res) => {
-  res.send('login');
+  res.send('login'); // Sends the "login" response for the `/login` path
 });
 
 app.get('/signup', (req, res) => {
-  res.send('signup');
+  res.send('signup'); // Sends the "signup" response for the `/signup` path
 });
 
+// Starts the server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-
-module.export = app
+module.exports = app;
