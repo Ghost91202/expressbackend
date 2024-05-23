46bad3ceef505c5cf91dfb5fd223bbb869264b4e
ghost91202 <karansharma8543@gmail.com>
2024-05-23 18:10:07 +0530

    set up project files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// const express = require('express'); // Imports the Express framework
+// const app = express(); // Creates an Express application instance
+
+// const port = 4000; // Uses the `PORT` environment variable if set, otherwise defaults to 4000
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('Hello World!'); // Sends the "Hello World!" response for the root path (`/`)
+// });
+
+// var users = [ {
+//   name: "karan"
+// }]
+
+
+// // Starts the server
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+// module.exports = app;
+
+
+// Advance backend set up 
